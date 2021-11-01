aa311da6f123b9365c58e87c765c85435f00399c
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-01 21:08:25 +0900

    ローディングの表示を追加した。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from 'next/app';
+import NextProgress from 'next-progress';
 import '../styles/globals.scss';
 import { BottomNavigation, Header, Footer } from '../components/index';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <div className='app'>
+            <NextProgress
+                color='#61dafb'
+                options={{ easing: 'ease', speed: 500 }}
+            />
             <Header />
             <main className='wrapper'>
                 <Component {...pageProps} />
